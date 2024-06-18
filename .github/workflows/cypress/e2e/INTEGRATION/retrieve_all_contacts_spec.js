be21912d26a94467c2112c38c944de5daa10bc94
Adeyinka <164424214+AdeyinkaMInah@users.noreply.github.com>
2024-06-18 02:39:08 -0500

    retrieve_all_contacts_spec.js
    Test 1: retrieve all contacts

---
+++ .github/workflows/cypress/e2e/INTEGRATION/retrieve_all_contacts_spec.js
@@ -0,0 +1,9 @@
+describe('Retrieve All Contacts', () => {
+  const baseUrl = 'https://thinking-tester-contact-list.herokuapp.com';
+
+  it('should retrieve all contacts', () => {
+    cy.request(`${baseUrl}/contacts`)
+      .its('status')
+      .should('eq', 200);
+  });
+});
